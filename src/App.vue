aafa1217c886d4b7d9aa43db9ac06dcb4845a5f7
Priefy <peachy-lovly-mom@hotmail.com>
2017-11-17 00:20:19 +0700

    install Bulma and redesign page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar is-transparent container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+        </a>
+        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="#">
+            Home
+          </a>
+          <a class="navbar-item" href="#">
+            Education&Skills
+          </a>
+          <a class="navbar-item" href="#">
+            Activities
+          </a>
+          <a class="navbar-item" href="#">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -18,6 +46,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
